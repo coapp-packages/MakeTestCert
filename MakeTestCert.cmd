a6782e35f4ec38af7fdda453dfe5d9980dc55c38
Garrett Serack <garretts@microsoft.com>
2012-02-17 15:04:59 -0800

    new package

---
+++ MakeTestCert.cmd
@@ -0,0 +1,52 @@
+@echo off
+setlocal 
+
+if "%*" == "" goto USAGE
+
+SET SUBJECTNAME=%*
+
+
+makecert -pe -ss MY -sr LocalMachine -$ individual -n "CN=%SUBJECTNAME%" -len 2048 -r "%SUBJECTNAME%.cer"
+CertMgr /add "%SUBJECTNAME%.cer" /s /r localMachine root
+certutil.exe -privatekey -exportpfx "%SUBJECTNAME%" "%SUBJECTNAME%.pfx"
+
+echo.
+echo.
+echo.
+echo.
+echo ===========================================================================
+
+echo Created Files: 
+dir /b "%SUBJECTNAME%.*"
+
+echo.
+echo.
+echo To install the certificate on a test machine, you can copy the 
+echo "%SUBJECTNAME%.CER" file to the test computer and run:
+echo.
+echo     CertMgr /add "%SUBJECTNAME%.cer" /s /r localMachine root
+echo.
+
+goto fin
+:USAGE
+echo Create Test Certificate Script
+echo. 
+echo Creates a certificate along with a private key for testing code signing
+echo.
+echo Will generate a .CER file (the public certificate that you can install on
+echo any machine)
+echo.
+echo And a .PFX file (the private key and certificate in a PKCS#12 file
+echo.
+echo Usage:
+echo %~f0 "subject name"
+echo.
+echo The utility will ask for a password when creating a .PFX file.
+echo.
+
+
+goto fin
+:FAILED
+
+goto fin
+:FIN
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Create Test Certificate Script
+ 
+### Requires Windows SDK 
+
+Creates a certificate along with a private key for testing code signing
+
+Will generate a .CER file (the public certificate that you can install on
+any machine)
+
+And a .PFX file (the private key and certificate in a PKCS#12 file
+
+Usage:
+%~f0 "subject name"
+
+The utility will ask for a password when creating a .PFX file.
+
+
+# How to create the package for this CMD script
+
+To create the package for this CMD script, you can simply call autopackage:
+
+```
+autopackage MakeTestCert.autopkg
+```
+
+If it is the first time you are using autopackage, you can pass the certificate
+and password to the it, and ask it to remember for next time:
+
+```
+autopackage --certificate-path=cert.pfx --password=pass --remember MakeTestCert.autopkg
+```
